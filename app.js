08d401b43201b6869bf7ebf523c0df42dcd5ffc4
itsabubakar <seeker99157@gmail.com>
2021-09-18 12:52:00 +0100

    added review notes

--- app.js
+++ app.js
@@ -35,17 +35,20 @@ image.addEventListener('click', (e)=> {
     currentImage++;
     if(currentImage > img.length - 1) {
         currentImage = 0;
+        console.log(img[currentImage].src);
         image.src = img[currentImage].src;
     } else {
         image.src = img[currentImage].src;
     }
-
-    // currentImage++;
-    // img.src = 
-    // img.map(image => {
-    //     console.log(image.src);
-    //     image.src;
-    // })
-
-    // image.src = img[0].src
 })
+
+
+/*
+    Img contains the link to all my img src. Its as if I am getting the files from a server. The only thing I'd have to do  is parse them as they would come as a json file.
+
+    I tried to loop through them with a for each but that didn't work as it gave me all the sources.
+
+    then I looked up Smilgas similar project and saw he created a counter variable which I he incremented or decremented every time next or prev was clicked.
+
+    And it was resetted after the currentImage was > the img.length
+*/
